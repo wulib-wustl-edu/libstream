1daaf0906b4242b8e2d006c5dedda6490820a1ab
Phil Suda <phil.suda@wustl.edu>
2018-05-10 13:11:14 -0500

    update title limit to 100 characters for validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,7 +2,7 @@ class Resource < ApplicationRecord
   has_hashids_uri salt: '1ac206225a7806', min_length: 7
 
   validates :title, presence: true,
-            length: { maximum: 50 }
+            length: { maximum: 100 }
   validates :item_id, presence: true, length: {maximum: 6}
   validates :instructor, presence: true, length: {maximum: 50}
 
