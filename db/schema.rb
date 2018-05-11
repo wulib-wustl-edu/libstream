d755d848c00848e44a267e41572b7b51732c5b4f
Phil Suda <phil.suda@wustl.edu>
2018-05-11 10:59:10 -0500

    fix video attribute in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501160907) do
+ActiveRecord::Schema.define(version: 20180511130718) do
 
   create_table "resources", force: :cascade do |t|
     t.string "item_id"
